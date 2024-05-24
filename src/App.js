e29c859008a711b0d112727e27769fdc86bba6bb
kirans2255 <kiransunilkumar2017@gmail.com>
2024-05-24 15:12:47 +0530

    react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import pic from './utils/images/pics.png';
 
-function App() {
+const Weddinginvitation = () => {
+  const [coupleNames, setCoupleNames] = React.useState({
+    bride: 'Stephanie',
+    groom: 'James'
+  });
+  const [date, setDate] = React.useState('June |25| 2024');
+  const [time, setTime] = React.useState("AT FIVE O'CLOCK IN THE EVENING");
+  const [location, setLocation] = React.useState('ROYAL BOTANICAL GARDEN - SYDENY');
+  const [sa, setSa] = React.useState('Request the pleasure of your company at the ceremony of our wedding');
+
+
+
+  const [backgroundImage, setBackgroundImage] = React.useState(pic);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='board'>
+      <div className="wedding-card">
+        <div className="background-image-container">
+          <img src={backgroundImage} alt="Wedding Background" />
+        </div>
+        <div className="card-content">
+          <h1 className="couple-names">
+            {coupleNames.bride} & {coupleNames.groom}
+          </h1>
+          <p className='sa'>{sa}</p>
+          <p className="date">{date} </p>
+          <p className="time">{time}</p>
+          <p className="location">{location}</p>
+        </div>
+      </div>
     </div>
   );
-}
+};
 
-export default App;
+export default Weddinginvitation;
