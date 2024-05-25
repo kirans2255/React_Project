9a39bf48a157cd62278e63efeb8fc10ac23440a5
kirans2255 <kiransunilkumar2017@gmail.com>
2024-05-25 18:59:23 +0530

    newupdate

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { useState } from 'react';
-import pic from './utils/images/pics.png';
 
 const Weddinginvitation = () => {
   const initialFormData = {
@@ -7,7 +6,8 @@ const Weddinginvitation = () => {
     groom: '',
     date: '',
     time: '',
-    location:''
+    location: '',
+    backgroundImage: null, // New state property for background image
   };
 
   const [formData, setFormData] = useState(initialFormData);
@@ -18,6 +18,18 @@ const Weddinginvitation = () => {
     setFormData({
       ...formData,
       [event.target.name]: event.target.value,
+    });
+  };
+
+  const handleBackgroundImageChange = (event) => {
+    const selectedFile = event.target.files[0];
+    // You can add validations here to check file size or format (optional)
+    // ...
+
+    // Assuming the file is valid, update state with the selected file
+    setFormData({
+      ...formData,
+      backgroundImage: selectedFile,
     });
   };
 
@@ -35,7 +47,11 @@ const Weddinginvitation = () => {
   return (
     <div className="wedding-invitation">
       <div className="background-image-container">
-        <img src={pic} alt="Wedding Background" style={{ width: '100%' }} />
+        {formData.backgroundImage ? (
+          <img src={URL.createObjectURL(formData.backgroundImage)} alt="Wedding Background" style={{ width: '100%' }} />
+        ) : (
+          <img src="./utils/images/pics.png" alt="Wedding Background" style={{ width: '100%' }} /> // Replace with your default image path
+        )}
       </div>
       {isEditing ? (
         <form onSubmit={handleSubmit}>
@@ -55,7 +71,11 @@ const Weddinginvitation = () => {
             <label htmlFor="location">Location:</label>
             <input type="location" id="location" name="location" value={formData.location} onChange={handleChange} required /> <br />
 
-            <button className='submit' type="submit">Submit</button>
+            <label htmlFor="backgroundImage">Background Image:</label>
+            <input type="file" id="backgroundImage" name="backgroundImage" accept="image/*" onChange={handleBackgroundImageChange} />
+            <br />
+
+            <button className="submit" type="submit">Submit</button>
           </div>
         </form>
       ) : (
@@ -67,7 +87,7 @@ const Weddinginvitation = () => {
           <p className="date">{submittedData ? submittedData.date : 'Date'}</p>
           <p className="time">{submittedData ? submittedData.time : 'Time'}</p>
           <p className="location">{submittedData ? submittedData.location : 'Location'}</p>
-          <button className='editbutton' onClick={toggleEditing}>Edit Details</button>
+          <button className="editbutton" onClick={toggleEditing}>Edit Details</button>
         </div>
       )}
     </div>
